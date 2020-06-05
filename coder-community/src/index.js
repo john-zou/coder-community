f531b9df71fb75788a57f389e93bad293e06233d
John Zou <extreme2202020@gmail.com>
2020-06-05 11:23:43 -0700

    Update index.js

--- coder-community/src/index.js
+++ coder-community/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -14,20 +14,17 @@ import { rootReducer } from "./reducers";
 const store = createStore(rootReducer);
 
 // Mina
-// ReactDOM.render(
-// <ThemeProvider theme={theme}>
-//     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-//     <CssBaseline />
-//     <App />
-//   </ThemeProvider>,
-//   document.querySelector('#root'),
-// );
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
+      </Provider>
+
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.querySelector('#root'),
 );
+
