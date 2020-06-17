b4ffbf40300a2a9cef9bd236af44ca19d1d275ce
John Zou <extreme2202020@gmail.com>
2020-06-16 17:23:03 -0700

    Use CRA's global CSS Normalize

--- coder-community/client/src/index.tsx
+++ coder-community/client/src/index.tsx
@@ -1,30 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
-import "./index.css";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { rootReducer } from "./reducers";
+import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers';
 
 const store = createStore(rootReducer);
 
-// Mina
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
         <App />
       </Provider>
-
     </ThemeProvider>
   </React.StrictMode>,
   document.querySelector('#root'),
 );
-
