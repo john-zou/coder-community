73a478ba8dddb8c7e992f4c33105a92da0de2443
John Zou <extreme2202020@gmail.com>
2020-06-20 23:24:57 -0700

    Create clientIDs.js

---
+++ backend/clientIDs.js
@@ -0,0 +1,6 @@
+// These are used by `AuthService` on the back end, and during the Login process on the front end
+// There are two copies of this file, one in `backend/src` and one in `frontend/src`
+
+export const GitHubOAuthClientID = 'f397e7312eb6205b3256';
+export const GoogleOAuthClientID =
+  '378315098234-caculcp2l4rg87nb7tds0j37v420cth9.apps.googleusercontent.com';
