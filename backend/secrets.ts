b5c47ff0fafa4852bfc54cf076d142095fc8b941
John Zou <extreme2202020@gmail.com>
2020-06-21 21:15:52 -0700

    Create stub secrets.ts

---
+++ backend/secrets.ts
@@ -0,0 +1,8 @@
+// You should only see this file in the remote repo. This should be replaced with the real version in your local repo.
+// (Check Slack)
+
+export const CoderCommunityJwtSecret = '';
+
+export const GitHubOAuthClientSecret = '';
+
+export const GoogleOAuthClientSecret = '';
