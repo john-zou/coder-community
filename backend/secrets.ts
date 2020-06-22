f8049e5a3f12fac1616e57051be5f544fd6da787
John Zou <extreme2202020@gmail.com>
2020-06-21 21:32:36 -0700

    Delete secrets.ts

--- backend/secrets.ts
+++
@@ -1,8 +0,0 @@
-// You should only see this file in the remote repo. This should be replaced with the real version in your local repo.
-// (Check Slack)
-
-export const CoderCommunityJwtSecret = '';
-
-export const GitHubOAuthClientSecret = '';
-
-export const GoogleOAuthClientSecret = '';
