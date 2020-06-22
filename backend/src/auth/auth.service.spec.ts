7f44d66dcec84e5f46aebfa168c9fa29121e85ec
John Zou <extreme2202020@gmail.com>
2020-06-21 20:01:39 -0700

    Create AuthService tests

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,18 +1,124 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { HttpService, HttpModule } from '@nestjs/common';
+import { LoginSuccess } from './auth.dto';
+import { GitHubOAuthClientID } from './constants';
+import { GitHubOAuthClientSecret } from '../../secrets';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
+import { UserModule } from '../user/user.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+
+const GitHubAccessTokenUrl = 'https://github.com/login/oauth/access_token';
+const GitHubApi = 'https://api.github.com/user';
+const FakeCoderCommunityJwtSecret = '123';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let httpService: HttpService;
+  let userService: UserService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule, // For social OAuth
+        JwtModule.register({ secret: FakeCoderCommunityJwtSecret }), // For signing CoderCommunity jwt
+        UserModule, // For creation of new user
+      ],
       providers: [AuthService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    httpService = module.get<HttpService>(HttpService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('GitHub auth', () => {
+    it('should return a LoginSuccess with a jwt that passes UserAuthGuard when a good code is provided', async () => {
+      const code = 'abc';
+      const state = 'efg';
+      const fakeGitHubToken = 'hij';
+      const fakeGitHubLogin = 'klm';
+      const isNewUser = true;
+      const createUserSpy = jest
+        .spyOn(userService, 'createOrUpdateUser')
+        .mockImplementation(() => {
+          return Promise.resolve(isNewUser);
+        });
+
+      jest
+        .spyOn(httpService, 'post')
+        .mockImplementation((url, data, config) => {
+          expect(url).toEqual(GitHubAccessTokenUrl);
+          expect(data).toEqual({
+            client_id: GitHubOAuthClientID,
+            client_secret: GitHubOAuthClientSecret,
+            code,
+            state,
+          });
+          expect(config.headers.Accept).toEqual('application/json');
+          return new Observable<AxiosResponse>(subscriber => {
+            subscriber.next({
+              data: { access_token: fakeGitHubToken },
+            } as AxiosResponse);
+            subscriber.complete();
+          });
+        });
+
+      jest.spyOn(httpService, 'get').mockImplementation((url, config) => {
+        expect(url).toEqual(GitHubApi);
+        expect(config.headers.Accept).toEqual('application/json');
+        expect(config.headers.Authorization).toEqual(
+          'token ' + fakeGitHubToken,
+        );
+        return new Observable<AxiosResponse>(subscriber => {
+          subscriber.next({
+            data: { login: fakeGitHubLogin },
+          } as AxiosResponse);
+          subscriber.complete();
+        });
+      });
+
+      const actual = await authService.authenticateGitHubLogin({ code, state });
+      expect(createUserSpy).toBeCalledTimes(1);
+      expect(jwtService.verify(actual.jwt).userID).toEqual(fakeGitHubLogin);
+      expect(actual.isNewUser).toEqual(isNewUser);
+      expect(actual.userID).toEqual(fakeGitHubLogin);
+    });
+
+    it('should throw HttpError and not create a user when GitHub rejects the code', async () => {
+      const code = 'abc';
+      const state = 'def';
+
+      const createUserSpy = jest.spyOn(userService, 'createOrUpdateUser');
+
+      jest
+        .spyOn(httpService, 'post')
+        .mockImplementation((url, data, config) => {
+          expect(url).toEqual(GitHubAccessTokenUrl);
+          expect(data).toEqual({
+            client_id: GitHubOAuthClientID,
+            client_secret: GitHubOAuthClientSecret,
+            code,
+            state,
+          });
+          expect(config.headers.Accept).toEqual('application/json');
+          return new Observable<AxiosResponse>(subscriber => {
+            subscriber.next({
+              status: 404,
+            } as AxiosResponse);
+            subscriber.complete();
+          });
+        });
+
+      await expect(
+        authService.authenticateGitHubLogin({ code, state }),
+      ).rejects.toThrow('GitHub');
+
+      expect(createUserSpy).toBeCalledTimes(0);
+    });
   });
 });
