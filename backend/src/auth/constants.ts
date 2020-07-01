68e9658bddd035daaca88cb4aa01f56ba1ea57be
john-zou <61474884+john-zou@users.noreply.github.com>
2020-07-01 10:47:32 -0700

    Mock MongoDB and Compodoc (#64)
    * Update package.json
    
    * Update yarn.lock
    
    * Update tsconfig.build.json
    
    * Remove console.log from constants.ts
    
    * Create reseed-mongodb.ts
    
    * Add Mocking of MongoDB/Typegoose for tests
    
    * Update README.md
    
    * Add MongoDB to GitHub action
    
    * Make Local MongoDB the default connection for GitHub Actions
    
    * Test cleanup in afterAll, afterEach

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -1,8 +1,6 @@
 import { Secrets } from "../secrets";
-
-console.log("constants.ts: ", Secrets);
-
 export const GitHubOAuthClientID = 'f397e7312eb6205b3256';
 export const GoogleOAuthClientID =
   '378315098234-caculcp2l4rg87nb7tds0j37v420cth9.apps.googleusercontent.com';
 export const MONGODB_URI = 'mongodb+srv://m001-student:' + Secrets.MONGODB_URI_PASSWORD + '@cluster0-wykga.mongodb.net/coderCommunity?retryWrites=true&w=majority';
+export const LOCAL_MONGODB = 'mongodb://localhost:27017/test'
