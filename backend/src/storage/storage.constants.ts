b05dc1a1ce34373565785ed1d292fc29ea90603d
John Zou <john.hua.zou@gmail.com>
2020-07-02 12:32:24 -0700

    Add validation to file upload

--- backend/src/storage/storage.constants.ts
+++ backend/src/storage/storage.constants.ts
@@ -1,2 +1,4 @@
 export const PublicUserContentServeRoot = "public"; // e.g. localhost:3001/public/profile-pic/23nklfn3afklnaf.jpg
 export const PublicUserContentDir = "public"; // backend/public/*
+
+export const FileSizeLimitInBytes = 20*1000*1000; // 20 MB
--- backend/src/upload/upload.module.ts
+++ backend/src/upload/upload.module.ts
@@ -4,11 +4,15 @@ import { UploadService } from './upload.service';
 import { StorageModule } from '../storage/storage.module';
 import { StorageService } from '../storage/storage.service';
 import { MulterModule } from '@nestjs/platform-express';
+import { FileSizeLimitInBytes } from '../storage/storage.constants';
+import { multerFileFilter } from './multer-file-filter';
 
 @Module({
   imports: [
     MulterModule.register({
       dest: './tmp',
+      fileFilter: multerFileFilter,
+      limits: {fileSize: FileSizeLimitInBytes}
     }),
     StorageModule,
   ],
