5809132275823d91acdb0b392d868cba41b78a23
John Zou <61474884+john-zou@users.noreply.github.com>
2020-07-04 18:23:46 -0700

    Minor env-related fixes (#81)
    * Minor env-related fixes
    
    * Update node.js.yml

--- backend/src/secrets.ts
+++ backend/src/secrets.ts
@@ -2,5 +2,7 @@ export const Secrets = {
   CoderCommunityJwtSecret: process.env.CC_SECRET,
   GitHubOAuthClientSecret: process.env.GH_SECRET,
   GoogleOAuthClientSecret: process.env.GG_SECRET,
-  MongoConnectionString: (process.env.CI || process.env.USE_LOCAL_MONGODB) ? process.env.LOCAL_MONGODB : process.env.REMOTE_MONGODB
+  MongoConnectionString: (process.env.CI || (process.env.USE_LOCAL_MONGODB && process.env.USE_LOCAL_MONGODB !== "false")) ? process.env.LOCAL_MONGODB : process.env.REMOTE_MONGODB
 };
+
+console.log(process.env);
