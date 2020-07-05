400539d7cce629a3732f6fde3139bf9baa507766
John Zou <61474884+john-zou@users.noreply.github.com>
2020-07-05 09:12:17 -0700

    GitHub Login and GitHub Account Info (#86)
    * Begin implementing auth on frontend
    
    * Update README.md
    
    * login
    
    * Log the MongoDB connection
    
    * Add storing of GitHub user info upon account creation
    
    * Add qs and uuid packages
    
    * Real GitHub login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,29 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Footer from './containers/footer/Footer';
-import Header from './containers/header/Header';
-import CreatePost from './pages/create_post/CreatePost';
-import Home from './pages/home';
-import { DevLogin } from './pages/login/DevLogin';
-import { Messenger } from './pages/messenger';
-import PostDetail from './pages/post_detail';
-import SearchResult from './pages/search_result/SearchResult';
-import Upload from './pages/video_management/Upload';
-import { ViewProfile } from './pages/view_profile/ViewProfile';
-import { LogOut } from './pages/login/Logout';
-import { Experimental } from './pages/experimental/Experimental';
+import Footer from "./containers/footer/Footer";
+import Header from "./containers/header/Header";
+import CreatePost from "./pages/create_post/CreatePost";
+import Home from "./pages/home";
+import { DevLogin } from "./pages/login/DevLogin";
+import { Messenger } from "./pages/messenger";
+import PostDetail from "./pages/post_detail";
+import SearchResult from "./pages/search_result/SearchResult";
+import Upload from "./pages/video_management/Upload";
+import { ViewProfile } from "./pages/view_profile/ViewProfile";
+import { LogOut } from "./pages/login/Logout";
+import { Experimental } from "./pages/experimental/Experimental";
+import { LoginGitHub } from "./pages/login/LoginGitHub";
 
 export type ViewProfileParams = {
   username: string;
-}
+};
 
 export type PostDetailParams = {
   slug: string;
-}
+};
 
 export default function App() {
   return (
@@ -52,6 +53,9 @@ export default function App() {
         </Route>
         <Route path="/video_management">
           <Upload />
+        </Route>
+        <Route path="/login/github">
+          <LoginGitHub />
         </Route>
         <Route path="/login">
           <DevLogin />
