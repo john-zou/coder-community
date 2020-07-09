14d9514b62f470fb4d7a134125d4be369f7bac8c
John Zou <61474884+john-zou@users.noreply.github.com>
2020-07-09 11:26:34 -0700

    upload related fixes (#116)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,5 @@
+import { Logger } from '@nestjs/common';
+
 require('dotenv').config();
 
 import { NestFactory } from '@nestjs/core';
@@ -6,10 +8,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { initializeMongo } from './mongoModels';
 import { Secrets } from './secrets';
+import { initializeStorageDirectories } from './storage/initializeStorageDirectories';
+
 
 async function bootstrap() {
   // Connect to MongoDB and set up Models.
   await initializeMongo(Secrets.MongoConnectionString);
+
+  initializeStorageDirectories();
 
   const app = await NestFactory.create(AppModule);
 
--- backend/src/storage/README.md
+++ backend/src/storage/README.md
@@ -1,3 +1,5 @@
 # `StorageModule`
 
 Stores user content locally. Will be replaced eventually by a cloud solution to scale the app.
+
+The upload controller / endpoints are handled by UploadModule.
