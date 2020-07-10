54f73ac40b6e02a727e8c8248bb2758220a7b059
yangybio <53034845+yangybio@users.noreply.github.com>
2020-07-09 23:13:53 -0700

    Video (#118)
    * video
    
    * video2

--- frontend/src/reducers/videosSlice.ts
+++ frontend/src/reducers/videosSlice.ts
@@ -1,3 +1,49 @@
-export function videos(state = {}, action) {
-  return state;
-}
+// export function videos(state = {}, action) {
+//   return state;
+// }
+
+import { PayloadAction } from '@reduxjs/toolkit';
+ import { PostsApi, VideoApi, GetAllVideosDto, VideoDto } from './../api/api';
+ import { Video } from './../store/types';
+ import { createEntityAdapter, createAsyncThunk } from '@reduxjs/toolkit';
+ import { createSlice } from '@reduxjs/toolkit';
+
+
+ const videosAdapter = createEntityAdapter<Video>({
+   selectId: video => video._id,
+ })
+
+ export const getAllVideos = createAsyncThunk(
+   'getAllVideosStatus',
+   async () => {
+     return await new VideoApi().videoControllerGetAllVideos();
+   }
+ );
+
+ export const createVideo = createAsyncThunk(
+   'createVideoStatus',
+   async (video: any) => {
+     const videoDto = {name: video.name, description: video.description};
+     return await new VideoApi().videoControllerCreateVideo(videoDto);
+   }
+ )
+
+ // dispatch(createVideo(video))
+
+ export const videosSlice = createSlice({
+   name: 'video',
+   initialState: videosAdapter.getInitialState(),
+   reducers: {
+
+   },
+   extraReducers: {
+     [getAllVideos.fulfilled.type]: (state, action: PayloadAction<GetAllVideosDto>) => {
+       const videosToAdd = action.payload.videos;
+       videosAdapter.upsertMany(state, videosToAdd);
+     },
+     [createVideo.fulfilled.type]: (state, action: PayloadAction<VideoDto>) => {
+       const video: Video = action.payload;
+       videosAdapter.addOne(state, video);
+     }
+   }
+ })
