4d4f926d534b2ea8dc8e28de9728f6f586e4d909
John Zou <john.hua.zou@gmail.com>
2020-07-11 11:53:09 -0700

    Redo Profile Card (now matches Figma exactly)

--- frontend/src/pages/view_profile/OwnProfile.tsx
+++ frontend/src/pages/view_profile/OwnProfile.tsx
@@ -5,15 +5,16 @@ import {CurrentLoggedInUser} from "../../store/types";
 import styled from '@emotion/styled';
 import {ProfileBanner} from "./ProfileBanner";
 import DefaultImg from "../../assets/defaultUserProfileBannerImg.jpg";
-
+import {TradingCard} from "./TradingCard";
 
 
 export function OwnProfile() {
     const user = useSelector<RootState, CurrentLoggedInUser>(state => state.user);
     const src = user.profileBanner || DefaultImg;
 
-    return <>
-        <ProfileBanner imgSrc={src} isUser={true}/>
+    return (<>
+        <ProfileBanner imgSrc={src}/>
+        <TradingCard user={user} isCurrentUser/>
         <h1>You are viewing your own profile, {user.userID}</h1>
-    </>;
+    </>);
 }
