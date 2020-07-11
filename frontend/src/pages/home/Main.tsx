9e30ca5601e3ef82834d191f2a0291d6c6391776
Dara Nguyen <duongnt1204@gmail.com>
2020-07-10 21:52:09 -0700

    Fetch posts for tags  (#123)
    * implemented leave group
    
    * handle join group
    
    * fixed group Ui styling
    
    * installed moment.js to format timestamp
    
    * added tags
    
    * added tags carousel
    
    * moved tags to top
    
    * done
    
    * added condition to stop fetching when there's no more post
    
    * Update TagsCarousel.tsx
    
    * updated style
    
    * Update RightSideBar.tsx
    
    * added fetching for individual tags

--- frontend/src/pages/home/Main.tsx
+++ frontend/src/pages/home/Main.tsx
@@ -4,11 +4,11 @@ import Card from './Card';
 import { RootState } from '../../reducers/rootReducer';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Loading } from '../common/Loading';
-import { fetchTrendingPosts } from '../../reducers/postsSlice';
+import { fetchPostsByTag, fetchTrendingPosts } from '../../reducers/postsSlice';
 import { AppDispatch } from '../../store';
-import { unwrapResult } from '@reduxjs/toolkit';
+import { Dictionary, unwrapResult } from '@reduxjs/toolkit';
 import { TagsCarousel } from './TagsCarousel';
-import { Post } from '../../store/types';
+import { Tag } from '../../store/types';
 
 //parent: Home
 const Main = () => {
@@ -17,33 +17,76 @@ const Main = () => {
   );
   const dispatch: AppDispatch = useDispatch();
   const [items, setItems] = useState(trendingPosts);//has 5 things initially
+  // 
   const currFetchCount: number = useSelector<RootState, number>(state => state.posts.trendingPostFetchCount);
-  const hasMorePosts: boolean = useSelector<RootState, boolean>(state => state.posts.hasMorePosts);
+  const hasMoreTrendingPosts: boolean = useSelector<RootState, boolean>(state => state.posts.hasMorePosts);
+  const [tabIndex, setTabIndex] = React.useState(0);
+  const tags = useSelector<RootState, Dictionary<Tag>>(state => state.tags.entities);
+  const hasMorePostsInTags = useSelector<RootState, Record<string, boolean>>(state => state.tags.hasMorePostsInTags);
+  const tagsArr = Object.values(tags);
+  const currentTag = tagsArr[tabIndex - 1];
+  const currentTagID = currentTag?._id;
+
+  let hasMore: boolean;
+  if (tabIndex === 0) {
+    hasMore = hasMoreTrendingPosts;
+  } else {
+    hasMore = hasMorePostsInTags[currentTagID];
+  }
+
+  const handleTabChange = (newIdx: number) => {
+    setTabIndex(newIdx);
+    // if new index is 0 (All - trending posts)
+    if (newIdx === 0) {
+      setItems(trendingPosts);
+      return;
+    }
+    const currentTag = tagsArr[newIdx - 1];
+    console.log(currentTag);
+    // switch to a tag
+    setItems(Object.keys(currentTag.postsSet));
+    const startIdx = items.length; // communicate to back end which ones to skip
+    const tagID = currentTagID;
+    dispatch(fetchPostsByTag({ tagID, startIdx })).then(unwrapResult).then(res => {
+      setItems(prev => prev.concat(res.posts.map(post => post._id)))
+    }).catch(err => console.log(err));
+  }
 
   const fetchMoreData = () => {
-    if (hasMorePosts) {
-      dispatch(fetchTrendingPosts({ fetchCount: currFetchCount })).then(unwrapResult).then(res => {
+    if (tabIndex === 0) {
+      if (hasMoreTrendingPosts) {
+        dispatch(fetchTrendingPosts({ fetchCount: currFetchCount })).then(unwrapResult).then(res => {
+          setItems(prev => prev.concat(res.posts.map(post => post._id)))
+        }).catch(err => console.log(err));
+      } else {
+        return;
+      }
+    }
+
+    const startIdx = items.length; // communicate to back end which ones to skip
+    const tagID = currentTagID;
+    if (hasMorePostsInTags[tagID]) {
+      dispatch(fetchPostsByTag({ tagID, startIdx })).then(unwrapResult).then(res => {
         setItems(prev => prev.concat(res.posts.map(post => post._id)))
-      }).catch(err => console.log(err))
-    } else {
-      console.log('no more post');
+      }).catch(err => console.log(err));
     }
   }
 
   return (
     <>
       <div style={{ display: "flex", position: "fixed", justifyContent: "center", width: "50%", zIndex: 10, marginTop: "-40px" }}>
-        <TagsCarousel />
+        <TagsCarousel value={tabIndex} setValue={handleTabChange} />
       </div>
       <div style={{ marginTop: "50px" }}>
         <InfiniteScroll
           dataLength={items.length} //This is important field to render the next data
           next={fetchMoreData}
-          hasMore={hasMorePosts}
+          hasMore={hasMore}
+          key={tabIndex.toString()}
           loader={<Loading />}
           endMessage={
             <p style={{ textAlign: 'center' }}>
-              <b></b>
+              <b>You've seen it all!</b>
             </p>
           }>
           {items.map((_id, idx) => (
--- frontend/src/pages/home/RightSideBar.tsx
+++ frontend/src/pages/home/RightSideBar.tsx
@@ -5,7 +5,8 @@ import { RootState } from '../../reducers/rootReducer';
 import { TinyButton as ScrollUpButton } from "react-scroll-up-button"; //https://www.npmjs.com/package/react-scroll-up-button
 import "./TagsCarousel.css";
 import { Dictionary } from '@reduxjs/toolkit';
-import { Post } from '../../store/types';
+import { Post, User } from '../../store/types';
+import Avatar from '../common/Avatar';
 
 const useStyles = makeStyles({
   root: {
@@ -18,12 +19,13 @@ const useStyles = makeStyles({
     marginRight: "5vw",
     position: "fixed",
     right: 0,
+
   },
   savePostSection: {
     display: "flex",
     flexDirection: "column",
     height: "50vh",
-
+    overflowY: "scroll",
   },
   savePostText: {
     marginBottom: "-1px",
@@ -44,31 +46,34 @@ export default function RightSideBar() {
   const trendingPosts = useSelector<RootState, string[]>(
     (state) => state.posts.trendingPosts
   );
+
   const posts = useSelector<RootState, Dictionary<Post>>(
     (state) => state.posts.entities
   );
-
+  const users = useSelector<RootState, Dictionary<User>>(
+    (state) => state.users.entities
+  );
   return (
     <div className={classes.root}>
       {isLoggedIn && <div>
         <p className={classes.savePostText}># POPULAR</p>
         <div className={classes.savePostSection}>
-          {/* {trendingPosts.map((post) => (
-            <div key={post.}>
-              <Avatar post={post} extraText=""></Avatar>
-              <Link
+          {trendingPosts.map((_id, idx) => (
+            <div key={idx}>
+              <Avatar pic={users[posts[_id].author].profilePic} title={posts[_id].title} subtitle={posts[_id].content.substr(0, 60).concat("...")} isButton={false} isPost={true} extraText={posts[_id].createdAt}></Avatar>
+              {/* <Link
                 to={`/post/${post.postID}`}
                 className={classes.link}
                 onClick={() => {
                   handleViewPost(currViewedPost, post);
                 }}
-              >
-                <p style={{ marginTop: "-0.5em", fontWeight: "bold" }}>
-                  {post.title}
-                </p>
-              </Link>
+              > */}
+              {/* <p style={{ marginTop: "-0.5em", fontWeight: "bold" }}>
+                {post.title}
+              </p> */}
+              {/* </Link> */}
             </div>
-          ))} */}
+          ))}
         </div>
 
         <p className={classes.savePostText}># WHO TO FOLLOW</p>
