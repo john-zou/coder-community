2d15638b12e6aa20a48ad26547690240bcb26ecc
John Zou <john.hua.zou@gmail.com>
2020-07-12 22:44:02 -0700

    Remove urlSlug for modifying search string because some search terms have symbols e.g. C#

--- backend/src/search/search.service.ts
+++ backend/src/search/search.service.ts
@@ -1,20 +1,18 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PostModel } from '../mongoModels';
 import { SearchResultDto } from './search.dto';
-import * as urlSlug from 'url-slug';
 
 @Injectable()
 export class SearchService {
 
   async search(query: string): Promise<SearchResultDto> {
-    // Use urlSlug to help sanitize this string. Go from "aa bb" => "aa-bb" => ["aa", "bb"]
-    const words = urlSlug(query).split('-');
-    if (words.length === 0) {
+    const trimmed = query?.trim();
+    if (trimmed?.length === 0) {
       throw new BadRequestException('Invalid search query');
     }
 
     // https://docs.mongodb.com/manual/text-search/
-    const posts = await PostModel.find({ $text: { $search: words[0] } }, { score: { $meta: "textScore" } })
+    const posts = await PostModel.find({ $text: { $search: trimmed } }, { score: { $meta: "textScore" } })
       .sort({ score: { $meta: "textScore" } })
       .lean(); // https://mongoosejs.com/docs/tutorials/lean.html
     return {
