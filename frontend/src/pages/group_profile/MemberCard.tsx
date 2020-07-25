14b700f618bddea528e5f28810beea5edd4fb2d7
John Zou <john.hua.zou@gmail.com>
2020-07-24 23:34:23 -0700

    Update MemberCard.tsx

--- frontend/src/pages/group_profile/MemberCard.tsx
+++ frontend/src/pages/group_profile/MemberCard.tsx
@@ -3,5 +3,5 @@ import {User} from "../../store/types";
 import Avatar from "../common/Avatar";
 
 export function MemberCard({user, isAdmin}: {user: User, isAdmin?: boolean}) {
-  return <Avatar pic={user.profilePic} subtitle={isAdmin && "Admin"} title={user.name} extraText={user.userID}/>
+  return <Avatar pic={user.profilePic} subtitle={isAdmin ? "Admin" : "Member"} title={user.name} extraText={user.userID}/>
 }
