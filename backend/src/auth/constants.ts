c93270a49db05f2f88edf8424f06edc8ed068f04
John Zou <john.hua.zou@gmail.com>
2020-07-25 09:03:05 -0700

    Update constants.ts

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -1,4 +1,4 @@
-export const GitHubOAuthClientID = 'f397e7312eb6205b3256';
+export const GitHubOAuthClientID = '1172e5f10b0d169a374a';
 export const GoogleOAuthClientID =
   '378315098234-caculcp2l4rg87nb7tds0j37v420cth9.apps.googleusercontent.com';
   
