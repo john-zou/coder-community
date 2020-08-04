0129c0a07a9211d40dfe4fbe48ea54cbaae99da1
Dara Nguyen <duongnt1204@gmail.com>
2020-08-04 16:39:07 -0700

    fixed merge conflicts

--- backend/src/GlobalRedirectForReact.ts
+++ backend/src/GlobalRedirectForReact.ts
@@ -1,17 +1,9 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, NotFoundException } from '@nestjs/common';
-<<<<<<< HEAD
-=======
 import { Response } from "express"
->>>>>>> master
 import * as path from 'path';
 
 @Catch(NotFoundException)
 export class GlobalRedirectForReact implements ExceptionFilter {
-<<<<<<< HEAD
-  catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
-=======
   catch(exception: HttpException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -19,7 +11,6 @@ export class GlobalRedirectForReact implements ExceptionFilter {
       response.sendStatus(404);
       return;
     }
->>>>>>> master
     response.sendFile(path.resolve('./build/index.html'));
   }
 }
